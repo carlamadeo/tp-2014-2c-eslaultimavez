d4457431bc5a63adb8cf566807b2de72a6faf17e
pgm1984 <pgm_1984@yahoo.com.ar>
2014-11-20 20:01:25 -0300

    Actualizo ruta set_library

--- set_library.sh
+++ set_library.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # Para que tenga efecto ejecutar asi: . ./set-ld_library_path.sh
 dir=../..
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${dir}/so-commons-library/Debug/
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${dir}/Commons/Debug/
+
