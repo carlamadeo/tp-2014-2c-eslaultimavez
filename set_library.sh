cbe174ff91779b6a2f1cbad359c784323f6270ca
pgm1984 <pgm_1984@yahoo.com.ar>
2014-12-16 23:06:03 -0300

    agrego .sh compilar

--- set_library.sh
+++ set_library.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Para que tenga efecto ejecutar asi: . ./set-ld_library_path.sh
+# Para que tenga efecto ejecutar asi: . ./set_library_.sh
 dir=../..
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${dir}/Commons/Debug/
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:./Commons/make/
 
